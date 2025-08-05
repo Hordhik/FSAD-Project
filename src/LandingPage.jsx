4225d46a2a3546e380639421ce5d603a3d622506
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-08-06 00:53:25 +0530

    Initial commit

---
+++ src/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./LandingPage.css" 
+import { Library } from './Library';
+import { Midland } from './Midland';
+import { Current } from './Current';
+export const LandingPage = () => {
+
+
+  return (
+    <div>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='d-flex flex-row justify-content-start card_0'>
+
+          <Library/>
+          <Midland/>
+          <Current/>
+
+            </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
